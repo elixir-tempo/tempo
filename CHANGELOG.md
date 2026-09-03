0172f85eb7c9da4aa5be31d67d58538777c46ee7
Kip Cole <kipcole9@gmail.com>
2026-09-03 13:03:48 +1000

    Release 1.6.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,6 @@
 # Changelog
 
-## [Unreleased]
+## [v1.6.2] — 2026-09-03
 
 ### Fixed
 
--- mix.exs
+++ mix.exs
@@ -13,7 +13,7 @@ defmodule Tempo.MixProject do
     )
   end
 
-  @version "1.6.1"
+  @version "1.6.2"
 
   def project do
     [
